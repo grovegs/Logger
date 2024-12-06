81808e2a6cb1c363f1202030ae94c765710ce89e
Gencer Güzeloğlu <59722791+GencerG@users.noreply.github.com>
2024-12-06 11:18:00 +0300

    feat: benchmark (#1)
    * feat: add benchmark scenario
    
    * refactor: change method names and warmup count
    
    * fix: typo
    
    * feat: add benchmark result
    
    * refactor: update gitignore

--- sandbox/Benchmark/Program.cs
+++ sandbox/Benchmark/Program.cs
@@ -1,7 +1,62 @@
-﻿    public class Program
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using GroveGames.Logger;
+
+using Microsoft.Extensions.Logging;
+
+using ZLogger;
+using ZLogger.Providers;
+
+[MemoryDiagnoser]
+[SimpleJob(launchCount: 1, warmupCount: 100, invocationCount: 1000, iterationCount: 1000)]
+public class Benchmark
+{
+    private FileLogger _fileLogger;
+    private ILogger<Benchmark> _zlogger;
+
+    [GlobalSetup]
+    public void Setup()
     {
-        public static void Main(string[] args)
+        _fileLogger = new FileLogger(new LogFileFactory(Environment.CurrentDirectory).CreateFile());
+        var factory = LoggerFactory.Create(builder =>
         {
-            
-        }
+            builder.SetMinimumLevel(LogLevel.Debug);
+
+            builder.AddZLoggerRollingFile(options =>
+                {
+                    options.UsePlainTextFormatter(formatter =>
+                        {
+                            formatter.SetPrefixFormatter($"{0}|{1}|", (in MessageTemplate template, in LogInfo info) => template.Format(info.Timestamp, info.LogLevel));
+                            formatter.SetExceptionFormatter((writer, ex) => Utf8StringInterpolation.Utf8String.Format(writer, $"{ex.Message}"));
+                        });
+                    options.FilePathSelector = (timestamp, sequenceNumber) =>
+                    $"logs/{timestamp:yyyy-MM-dd}_{sequenceNumber:000}.log";
+                    options.RollingInterval = RollingInterval.Day;
+                    options.RollingSizeKB = 1024;
+                });
+        });
+
+        _zlogger = factory.CreateLogger<Benchmark>();
     }
+
+    [Benchmark]
+    public void FileLogger()
+    {
+        _fileLogger.Info("Test Tag", "This is a test message");
+    }
+
+    [Benchmark]
+    public void ZLogger()
+    {
+        _zlogger.LogInformation("This is a test message");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Benchmark>();
+    }
+}
