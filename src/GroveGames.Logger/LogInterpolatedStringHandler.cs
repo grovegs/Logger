62e77e1d9b3d44e41c8200841928fe467d0e0d66
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-01-29 19:03:22 +0300

    Create LogInterpolatedStringHandler.cs

---
+++ src/GroveGames.Logger/LogInterpolatedStringHandler.cs
@@ -0,0 +1,80 @@
+using System.Runtime.CompilerServices;
+
+namespace GroveGames.Logger;
+
+[InterpolatedStringHandler]
+public ref struct LogInterpolatedStringHandler
+{
+    private readonly Span<char> _buffer;
+    private int _position;
+
+    public readonly Span<char> Written => _buffer[.._position];
+
+    public LogInterpolatedStringHandler(int literalLength, int formattedCount, Span<char> buffer)
+    {
+        _buffer = buffer;
+        _position = 0;
+    }
+
+    public bool AppendLiteral(ReadOnlySpan<char> s)
+    {
+        return TryWrite(s);
+    }
+
+    public bool AppendFormatted<T>(T value) where T : ISpanFormattable
+    {
+        return value.TryFormat(_buffer[_position..], out int written, default, default) && Advance(written);
+    }
+
+    public bool AppendFormatted<T>(T value, string? format) where T : ISpanFormattable
+    {
+        return value.TryFormat(_buffer[_position..], out int written, format, default) && Advance(written);
+    }
+
+    public bool AppendFormatted(ReadOnlySpan<char> value)
+    {
+        return TryWrite(value);
+    }
+
+    private bool TryWrite(ReadOnlySpan<char> value)
+    {
+        if (_buffer.Length - _position < value.Length)
+        {
+            return false;
+        }
+
+        value.CopyTo(_buffer[_position..]);
+        _position += value.Length;
+        return true;
+    }
+
+    private bool Advance(int count)
+    {
+        if (_buffer.Length - _position < count)
+        {
+            return false;
+        }
+
+        _position += count;
+        return true;
+    }
+}
+public class Example
+{
+    public void FormatExample()
+    {
+        Span<char> buffer = stackalloc char[100];
+
+        int number = 42;
+        decimal price = 123.45m;
+        DateTime now = DateTime.Now;
+
+        if (SpanFormatter.TryFormat(buffer,
+            $"Number: {number}, Price: {price:C2}, Date: {now:d}",
+            out var formatted))
+        {
+            Console.WriteLine(formatted.ToString());
+        }
+    }
+}
+
