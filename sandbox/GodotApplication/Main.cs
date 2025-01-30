d321af799ed3bf490f4f13f045fa66a692760d3f
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-01-30 20:55:11 +0300

    test: fix sandbox compiler errors

--- sandbox/GodotApplication/Main.cs
+++ sandbox/GodotApplication/Main.cs
@@ -6,13 +6,12 @@ public partial class Main : Node2D
 {
     public override void _Ready()
     {
-        GodotFileLogger.Shared.AddProcessor(new GodotLogProcessor());
-        GodotFileLogger.Shared.Warning("test", "test");
-        GodotFileLogger.Shared.Error("test", "test");
+        GodotLogger.Shared.Warning("test", $"test");
+        GodotLogger.Shared.Error("test", $"test");
 
-        for (var i = 0; i< 10; i++)
+        for (var i = 0; i < 10; i++)
         {
-             GodotFileLogger.Shared.Info("test", $"{i}");
+            GodotLogger.Shared.Info("test", $"{i}");
         }
     }
 }
