5e2fba59bd25936e942aab8974fe2eabb2774b26
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-02-10 00:03:29 +0300

    fix: change return type of CreateLogger method from ILogger to Logger

--- src/GroveGames.Logger.Godot/GodotLoggerFactory.cs
+++ src/GroveGames.Logger.Godot/GodotLoggerFactory.cs
@@ -2,7 +2,7 @@ namespace GroveGames.Logger;
 
 public sealed class GodotLoggerFactory
 {
-    public static ILogger CreateLogger(Action<ILoggerBuilder> configure)
+    public static Logger CreateLogger(Action<ILoggerBuilder> configure)
     {
         var minLevel = GodotSettings.MinLogLevel.Value;
         var builder = new LoggerBuilder();
